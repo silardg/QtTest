caaa8d8d9b5432422c5022ae1defe3fd0dfb299b
Silard Gal <silard.gal@htecgroup.com>
2023-05-23 14:02:31 +0200

    Adding serial scanning and opening with hard coded

--- temperature/Temperature/serial.cpp
+++ temperature/Temperature/serial.cpp
@@ -2,9 +2,12 @@
 
 #include <QDebug>
 
+/**
+ * @brief serial::serial
+ * @param port
+ */
 serial::serial(QSerialPort *port) {
     m_serial = port;
-
 
     const auto serialPortInfos = QSerialPortInfo::availablePorts();
     qDebug() << serialPortInfos.size();
@@ -24,7 +27,58 @@ serial::serial(QSerialPort *port) {
                          ? QByteArray::number(portInfo.productIdentifier(), 16)
                          : QByteArray());
     }
+
+    // testing hardcoded
+    m_port_chosen = serialPortInfos[0];
+    open();
 }
 
+/**
+ * @brief serial::open
+ * @return 1 on successful open, 0 on failed open
+ */
+bool serial::open() {
+
+    // set the name from the port chosen
+    m_serial->setPortName(m_port_chosen.portName());
+
+    // set the parameters. If they are not set to new, default is used
+    m_serial->setBaudRate(m_config_baud);
+    m_serial->setDataBits(m_config_databits);
+    m_serial->setParity(m_config_parity);
+    m_serial->setStopBits(m_config_stopbit);
+    m_serial->setFlowControl(m_config_flow);
+
+    // try to open it
+    if (m_serial->open(m_config_openMode)) {
+        qInfo() << "Opened port at " << m_port_chosen.portName();
+        return 1;
+    }
+
+    qInfo() << "Couldn't open";
+    return 0;
+}
+
+/**
+ * @brief serial::close
+ */
+void serial::close() {
+    // if it is open, force close it
+    if (m_serial->isOpen())
+        m_serial->close();
+
+    qInfo() <<  "Closed port at " <<  m_port_chosen.portName();
+}
+
+//void serial::readAll() {
+
+//}
+
+//void serial::error() {
+
+//}
+
 serial::~serial() {
+    // on destruction, always close it
+    close();
 }
--- temperature/Temperature/serial.h
+++ temperature/Temperature/serial.h
@@ -4,13 +4,31 @@
 #include <QSerialPort>
 #include <QSerialPortInfo>
 
+#include <QVector>
+
 class serial
 {
 public:
     serial(QSerialPort *port);
     ~serial();
+
+    bool open();
+    void close();
 private:
+    // store the serial object
     QSerialPort *m_serial = nullptr;
+
+    // the port info that is being used
+    QSerialPortInfo m_port_chosen;
+
+    // default is 9600
+    int m_config_baud = QSerialPort::Baud9600;
+    QSerialPort::DataBits m_config_databits = QSerialPort::Data8;
+    QSerialPort::Parity m_config_parity = QSerialPort::EvenParity;
+    QSerialPort::StopBits m_config_stopbit = QSerialPort::OneStop;
+    QSerialPort::FlowControl m_config_flow = QSerialPort::NoFlowControl;
+    QIODevice::OpenModeFlag m_config_openMode = QIODevice::ReadWrite;
+
 };
 
 #endif // SERIAL_H
